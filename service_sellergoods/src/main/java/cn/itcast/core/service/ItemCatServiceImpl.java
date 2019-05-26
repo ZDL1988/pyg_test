ef5afc87d122d7bf374724663bd3bd4321e10259
ZQ <290355416@qq.com>
2019-05-26 23:50:34 +0800

    添加了商家后台-分类申请

--- service_sellergoods/src/main/java/cn/itcast/core/service/ItemCatServiceImpl.java
+++ service_sellergoods/src/main/java/cn/itcast/core/service/ItemCatServiceImpl.java
@@ -1,15 +1,19 @@
 package cn.itcast.core.service;
 
 import cn.itcast.core.dao.item.ItemCatDao;
+import cn.itcast.core.pojo.entity.ItemCatEntity;
 import cn.itcast.core.pojo.item.ItemCat;
 import cn.itcast.core.pojo.item.ItemCatQuery;
 import cn.itcast.core.util.Constants;
 import com.alibaba.dubbo.config.annotation.Service;
+import org.bouncycastle.asn1.x509.X509ObjectIdentifiers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.*;
+
+import static org.bouncycastle.asn1.x509.X509ObjectIdentifiers.id;
 
 @Service
 @Transactional
@@ -53,4 +57,39 @@ public class ItemCatServiceImpl implements  ItemCatService {
     public List<ItemCat> findAll() {
         return catDao.selectByExample(null);
     }
+
+    @Override
+    public void add(ItemCatEntity itemCatEntity) {
+        //新添加分类审核状态为"0"
+        itemCatEntity.setAuditStatus("0");
+        long id = (long) (Math.random() * 1000000);
+        itemCatEntity.setId(id);
+        String name = itemCatEntity.getName();
+        Long parentId = itemCatEntity.getParentId();
+        Long typeId = itemCatEntity.getTypeId();
+        ItemCat itemCat = new ItemCat();
+        itemCat.setName(name);
+        itemCat.setParentId(parentId);
+        itemCat.setTypeId(typeId);
+        redisTemplate.boundHashOps(Constants.REDIS_ITEMCAT_ENTITY).put(id,itemCatEntity);
+        Object o = redisTemplate.boundHashOps(Constants.REDIS_ITEMCAT_ENTITY).get(id);
+        System.out.println(o);
+    }
+
+    @Override
+    public List<ItemCatEntity> findItemCatestatus(String status) {
+        //遍历所有的集合
+        List<ItemCatEntity> list=new ArrayList<>();
+        Map entries = redisTemplate.boundHashOps(Constants.REDIS_ITEMCAT_ENTITY).entries();
+        Collection<ItemCatEntity> values = entries.values();
+
+        for (ItemCatEntity itemCatEntity : values) {
+            if (status.equals(itemCatEntity.getAuditStatus())){
+                list.add(itemCatEntity);
+            }
+        }
+        return list;
+    }
+
+
 }
--- web_shop/src/main/java/cn/itcast/core/controller/ItemCatController.java
+++ web_shop/src/main/java/cn/itcast/core/controller/ItemCatController.java
@@ -1,12 +1,18 @@
 package cn.itcast.core.controller;
 
+import cn.itcast.core.pojo.entity.ItemCatEntity;
+import cn.itcast.core.pojo.entity.PageResult;
+import cn.itcast.core.pojo.entity.Result;
+import cn.itcast.core.pojo.good.Brand;
 import cn.itcast.core.pojo.item.ItemCat;
 import cn.itcast.core.service.ItemCatService;
 import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 分类管理
@@ -40,5 +46,39 @@ public class ItemCatController {
         List<ItemCat> all = catService.findAll();
         return all;
     }
+
+
+
+    /**
+     * 添加
+     * @param itemCatEntity 添加的品分类对象
+     * @return
+     */
+    @RequestMapping("/add")
+    public Result add(@RequestBody ItemCatEntity itemCatEntity) {
+        try {
+            catService.add(itemCatEntity);
+            return new Result(true, "保存成功!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "保存失败!");
+        }
+    }
+
+
+    @RequestMapping("/findItemCatestatus")
+  public List<ItemCatEntity> findItemCatestatus(String status){
+        List<ItemCatEntity> list= catService.findItemCatestatus(status);
+        return list;
+
+  }
+
+
+
+
+
+
+
+
 
 }
